0b2d39b6429b5a9ca0575efa2b2dad6001865376
Mikhail Sokolov <karsat86@mail.ru>
2017-02-05 14:50:59 +0600

    Update Form1.cs

--- HM3/Form1.cs
+++ HM3/Form1.cs
@@ -13,6 +13,7 @@ namespace HM3
     public partial class Form1 : Form
     {
         static String1 StringEditor1 = new String1();
+        //Лучше чтобы коллекция сначала была пустой, а значения добавлять уже при нажатии на кнопку
         List<String1.Filter> strfiletr = new List<String1.Filter> { StringEditor1.UpperRegister, StringEditor1.Revers, StringEditor1.SpaceAdd };
         public Form1()
         {
@@ -21,8 +22,10 @@ namespace HM3
         private void button1_Click(object sender, EventArgs e)
         {
             OutputTextBox.Text = InputTextBox.Text;
+            if (checkUpper.Checked)//можно сократить вот так
             if (checkUpper.Checked == true)
             {
+                //дублирование кода. Сначала нужно добавить в strfiletr значения и вызвать ровно 1 раз GetFiltered
                 OutputTextBox.Text = StringEditor1.GetFiltered(OutputTextBox.Text, strfiletr[0]);
             }
             if (checkRevers.Checked == true)
